ba21f92f32bf1f441aa4ff572a81f80d7ee22cc2
Utkarsh Patel <pateluttkarsh697@gmail.com>
2022-12-23 04:58:39 +0530

    added the profile header

--- WhatsApp-Clone/src/App.jsx
+++ WhatsApp-Clone/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProfileHeader from './components/ProfileHeader'
 
 const App = () => {
   return (
     <div>
-      
+      <h1 className='caret-red-500'>hello</h1>
+      <ProfileHeader/>
     </div>
   )
 }
